5b2e81e9f6c74e20a599f26ef7ae255d1d7517b7
iamfeysal <iamfeysal@gmail.com>
2019-09-12 03:15:28 +0300

    deployed

--- asp/settings.py
+++ asp/settings.py
@@ -128,7 +128,7 @@ DATABASES = {
         'NAME': 'asp',
         'USER': 'root',
         'PASSWORD': 'fazmandinho',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '6122',
     }
 }
