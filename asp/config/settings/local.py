82a31281c2827f95e4ee9dfe382beab1ce18b0ab
fatahrez <fertahrez@gmail.com>
2019-11-17 18:51:53 +0300

    alter settings/local.py

--- asp/config/settings/local.py
+++ asp/config/settings/local.py
@@ -1,29 +1,25 @@
-from asp.config.settings.base import * #noqa
-
+from asp.config.settings.base import *  # noqa
 
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
 )
 
-
 CORS_ORIGIN_ALLOW_ALL = True
-
-
 
 DATABASES = {
     'default': dj_database_url.config(
     )
 }
 
-if config('MODE')=="dev":
+if config('MODE') == "dev":
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'asp',
-            'USER': 'root',
-            'PASSWORD': 'fazmandinho',
-            'HOST': 'localhost',
+            'NAME': config("DB_NAME"),
+            'USER': config("DB_USER"),
+            'PASSWORD': config("DB_PASSWORD"),
+            'HOST': config("DB_HOST"),
             'PORT': '',
         }
     }
@@ -36,8 +32,6 @@ else:
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
