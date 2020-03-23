fc4471fba542c4de7a5e465590b043db2038bfb1
iamfeysal <iamfeysal@gmail.com>
2020-03-24 01:30:57 +0300

    add django heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn asp.wsgi --log-file -
+web: gunicorn asp.wsgi
--- asp/config/settings/base.py
+++ asp/config/settings/base.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -208,3 +208,4 @@ EMAIL_HOST_PASSWORD = 'fumzrnzrlezfgryl'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
+django_heroku.settings(locals())
