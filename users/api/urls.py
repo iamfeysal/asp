752e5da7d0ff5aa70505fa3dcf4c66c384a59180
iamfeysal <iamfeysal@gmail.com>
2020-07-08 22:55:16 +0300

    add homepage for doc purposes

--- users/api/urls.py
+++ users/api/urls.py
@@ -1,6 +1,5 @@
-from django.urls import path,re_path, include
+from django.urls import path, re_path, include
 from rest_framework.authtoken.views import obtain_auth_token
-
 
 from rest_framework import routers
 
@@ -9,20 +8,14 @@ from users.api.views import UserFeedbackViewSet, ExploreCoaches, \
 
 router = routers.DefaultRouter()
 router.register(r'feedback', UserFeedbackViewSet)
-# router.register(r'devices', DeviceViewSet)
-# router.register(r'messagetemplate', CreateMessageTemplateViewSet)
-# router.register(r'mailalerts', MailAlertsViewSet)
-# router.register(r'mailsubscription', MailSubscriptionViewSet)
-
 
 urlpatterns = [
-    # path('sign_up', SignUp, name='sign_up'),
     re_path('', include(router.urls)),
     re_path(r'^auth-token$', obtain_auth_token),
     path("explore_coaches/", ExploreCoaches.as_view(), name="explore_coaches"),
     path("explore_players/", ExplorePlayers.as_view(), name="explore_players"),
     path("<int:user_id>/follow/", FollowUser.as_view(), name="follow_user"),
-    path("<int:user_id>/unfollow/", UnFollowUser.as_view(), 
+    path("<int:user_id>/unfollow/", UnFollowUser.as_view(),
          name="unfollow_user"),
     path("<first_name>/followers", UserFollowers.as_view(),
          name="user_followers"),
