a4badfb1d0f8ed69534f208b3af3f01c44602079
iamfeysal <iamfeysal@gmail.com>
2020-07-08 21:47:56 +0300

    add homepage for doc purposes

--- asp/config/settings/base.py
+++ asp/config/settings/base.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'whitenoise.runserver_nostatic',
     'django_jenkins',
+    'bootstrap4'
     
     # my apps
     'authentication.apps.UsersConfig',
