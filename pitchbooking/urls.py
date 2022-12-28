f983cbc094fc75492927d05bd5e0f2b8eae0ee30
feysal <iamfeysal@gmail.com>
2022-12-28 17:25:47 +0300

    add admin section and its url and views

--- pitchbooking/urls.py
+++ pitchbooking/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from pitchbooking.views import BookingCreateWizardView
+from pitchbooking.views import BookingCreateWizardView, BookingHomeView, \
+    BookingListView, BookingSettingsView,BookingDeleteView, BookingApproveView, get_available_time
 
 urlpatterns = [
     # path('', list_futsal_pitch, name='list_futsal'),
     # path('booking/', book_futsal, name='book_futsal'),
     path("", BookingCreateWizardView.as_view(), name="create_booking"),
+    path("admins", BookingHomeView.as_view(), name="admin_dashboard"),
+    path("admins/list", BookingListView.as_view(), name="booking_list"),
+    path("admins/settings", BookingSettingsView.as_view(), name="booking_settings"),
+    path("admins/<pk>/delete",
+         BookingDeleteView.as_view(), name="booking_delete"),
+    path("admins/<pk>/approve",
+         BookingApproveView.as_view(), name="booking_approve"),
+    path("get-available-time", get_available_time, name="get_available_time"),
 ]
