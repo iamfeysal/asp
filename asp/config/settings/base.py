ee9922ac569ae4a22978a9ee845f42ef109457a2
feysal <iamfeysal@gmail.com>
2023-01-07 20:50:12 +0300

    refactor

--- asp/config/settings/base.py
+++ asp/config/settings/base.py
@@ -3,7 +3,7 @@ import os
 from decouple import config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = config('SECRET_KEY'),
+SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG')
 
